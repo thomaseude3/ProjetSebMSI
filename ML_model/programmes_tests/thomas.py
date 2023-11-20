acef5fd263d64a58a88c5b0ea0e9588b0fcafe48
thomaseude3 <thomas.eude3@gmail.com>
2023-11-20 17:41:08 +0100

    programme pour lancer le modèle sur 2 photos et les déplacer dans un autre dossier tout en supprimant les dossiers "predict"

--- ML_model/programmes_tests/thomas.py
+++ ML_model/programmes_tests/thomas.py
@@ -1,14 +1,30 @@
 from ultralytics import YOLO
-import numpy
+import cv2
+import shutil
 
 # load a pretrained YOLOv8n model
 model = YOLO("../models/best (1).pt", "v8")
 
-# predict on an image
-detection_output = model.predict(source="test_ml.png", conf=0.25, save=True, project="../acquisition_image")
+image1 = "produit1.png"
+image2 = "produit2.png"
+nouveau_dossier = '../../acquisition_image/yolo_frames/'
+
+
+detection_output1 = model.predict(source=image1, conf=0.25, save=True, project="../../")
+detection_output2 = model.predict(source=image2, conf=0.25, save=True, project="../../")
 
 # Display tensor array
-print(detection_output)
+#print(detection_output)
 
-# Display numpy array
-print(detection_output[0].numpy())
+path1 = "../../predict/produit1.png"
+path2 = "../../predict2/produit2.png"
+
+
+# Déplacer l'image vers le nouveau dossier
+shutil.move(path1, nouveau_dossier + "image_analysee1.png")
+shutil.move(path2, nouveau_dossier + "image_analysee2.png")
+
+
+# Supprimer le dossier "predict"
+shutil.rmtree("../../predict")
+shutil.rmtree("../../predict2")
